bc80f076d0c1da5e69ad50e2012a0c209cc838f0
Jaideep singh thakur <88130081+jaideep0101@users.noreply.github.com>
2023-04-13 13:33:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ const fileName = "myfile.txt";
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  fs.updateFile(fileName,fileContent,(error)=>{
+  if(error){
+  console.log(error);
+  }else{
+     console.log(`${fileName} ${fileContent}`);
+  }
+    
+  })
   
 };
 
